5cb6bb84608baf24e8d637a8c8c6788b17ae1e71
owenhudsondesign <owenhudsondesign@gmail.com>
2025-10-28 17:29:40 -0400

    Remove Stripe API version to use SDK default

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,7 +2,6 @@ import Stripe from 'stripe';
 
 // Initialize Stripe with secret key
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2024-12-18.acacia',
   typescript: true,
 });
 
